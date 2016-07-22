1bb2572c28343cc87ea85f22f60b36e636565873
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-22 09:41:04 -0400

    add delete api

--- threads/api_views.py
+++ threads/api_views.py
@@ -2,7 +2,14 @@ from rest_framework import generics
 from .models import Posts
 from .serializers import PostSerializer
 
+
 class PostUpdateView(generics.UpdateAPIView):
+
+    serializer_class = PostSerializer
+    queryset = Posts.objects.all()
+
+
+class PostDeleteView(generics.DestroyAPIView):
 
     serializer_class = PostSerializer
     queryset = Posts.objects.all()
