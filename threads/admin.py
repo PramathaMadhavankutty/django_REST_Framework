4b1db066ccc1af415682d6919807a6369874f512
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-22 08:12:48 -0400

    format

--- threads/admin.py
+++ threads/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Subject,Thread,Posts
+from .models import Subject, Thread, Posts
 # Register your models here.
 admin.site.register(Subject)
 admin.site.register(Thread)
