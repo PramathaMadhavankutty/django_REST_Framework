e10b0b305e1d907791c34d0236300223ef0d067a
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-22 07:31:32 -0400

    create update api view

---
+++ threads/api_views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Posts
+from .serializers import PostSerializer
+
+class PostUpdateView(generics.UpdateAPIView):
+
+    serializer_class = PostSerializer
+    queryset = Posts.objects.all()
